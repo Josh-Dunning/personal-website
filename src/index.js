e880ddbb47842f4ad1a8d8b7a460f3be6f68d947
Josh-Dunning <Josh.Dunning@rice.edu>
2017-08-05 22:45:33 -0500

    Attached new nav component to header

--- src/index.js
+++ src/index.js
@@ -2,23 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Transition } from 'semantic-ui-react';
+import $ from 'jquery';
+import Nav from './Nav';
 
 class Head extends React.Component {
 
 	constructor() {
 		super();
 		this.state = { 
-			visibility: []
+			visibility: [],
+			navPages: ['home', 'projects', 'about', 'blog', 'github', 'contact'],
+			navIndex: '',
+			images: {
+				home: require('./images/home.png'),
+				projects: require('./images/projects.png'),
+				about: require('./images/about.png'),
+				blog: require('./images/blog.png'),
+				github: require('./images/github.png'),
+				contact: require('./images/contact.png'),
+			}
+
 		};
 
-		for ( var i = 0; i < 6; i++) {
+		for ( var i = 0; i < this.state.navPages.length; i++) {
 			this.state.visibility.push(false),
 			this.setState({visibility: this.state.visibility })
 		}
 	}
 
-	showVisibility(i) {
-		this.state.visibility[i] = true,
+	toggleVisibility(i, bool) {
+		this.state.visibility[i] = bool,
 		this.setState({visibility: this.state.visibility});
 	}
 
@@ -26,10 +39,23 @@ class Head extends React.Component {
   		return string.charAt(0).toUpperCase() + string.slice(1);
   	}
 
+  	componentDidMount () {
+  		$("a").hover(
+	    function(event) {
+	        // The mouse has entered the element, can reference the element via 'this'
+	        //console.log(this)
+	    },
+	    function (event) {
+	        // The mouse has left the element, can reference the element via 'this'
+	    }
+ 	);
+  }
+
+  	
+
   render() {
 
-	var navPages = ['home', 'projects', 'about', 'blog', 'github', 'contact'];
-
+/*
 	let images = {
 		home: require('./images/home.png'),
 		projects: require('./images/projects.png'),
@@ -37,9 +63,11 @@ class Head extends React.Component {
 		blog: require('./images/blog.png'),
 		github: require('./images/github.png'),
 		contact: require('./images/contact.png'),
-	}
+	}*/
 
 	const { visibility } = this.state
+	const { navPages } = this.state
+	const { images } = this.state
 
     return ( 
       
@@ -55,29 +83,7 @@ class Head extends React.Component {
 	        </div>
 			{navPages.map((page, i) => (
 			<div key={page}>
-			 <a 
-			  href={ page === 'github' ? ('https://github.com/Josh-Dunning') : (page + '.html')}
-			  style={{display: 'block'}}
-			  target={ page === 'github' ? "_blank" : ""}
-			  onMouseOver={() => this.showVisibility(i)}>
-	          <div className="navBox">
-	            <div className="navBand"></div>          
-	            <div className="circBox">
-	              <span className="circ">
-	                <img src={images[page]} alt='missing_image' style={{height: '25px'}}/>
-	              </span>
-	            </div>
-	            <span className="navCover">
-	              <div className="navText">{this.capitalize(page)}</div>
-	            </span>
-	            <div className="circBoxB">
-	              <div className="circB"></div>
-	            </div>
-	          </div>
-	        </a>
-	        <Transition visible={visibility[i]} animation='drop' duration={700}>
-	        	<div className="dropBox">AA</div>
-	        </Transition>
+			  <Nav name={page} image={images[page]}></Nav>
 	        </div>)
 			)}
 	      </div>
